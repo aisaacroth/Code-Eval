afa79f1a8b92f1e71aba340ae8e31f27301a3ee5
aisaacroth <aisaacroth@gmail.com>
2015-06-01 22:04:34 -0400

    Working on the C++ version now

---
+++ Fizz-Buzz/C++/fizz_buzz.cpp
@@ -0,0 +1,5 @@
+#include "fizz_buzz.h"
+
+int main(int argc, char *argv[]) {
+    return 0;
+}
---
+++ Fizz-Buzz/C++/fizz_buzz.h
@@ -0,0 +1,7 @@
+#ifndef __FIZZ_BUZZ_H__
+#define __FIZZ_BUZZ_H__
+
+#include <fstream>
+#include <iostream>
+
+#endif
