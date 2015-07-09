092bd26de27263d1715b8f226f03eab6d574ef7d
aisaacroth <aisaacroth@gmail.com>
2015-07-09 18:11:40 -0400

    Working on the cpp answer

--- Fizz-Buzz/C++/fizz_buzz.cpp
+++ Fizz-Buzz/C++/fizz_buzz.cpp
@@ -1,5 +1,30 @@
 #include "fizz_buzz.h"
 
-int main(int argc, char *argv[]) {
+static void print_args(char *argv[])
+{
+    std::cerr << "usage: " << argv[0] << " <filename>" << std::endl;
+    std::exit(EXIT_FAILURE);
+}
+
+int main(int argc, char *argv[]) 
+{
+    
+    if (argc != 2) {
+        print_args(argv);
+    }
+
+    std::string line;
+    std::ifstream test_file("test.txt");
+
+    if(test_file.is_open()) {
+        while(getline(test_file, line)) {
+            std::cout << line << std::endl;
+        }
+
+        test_file.close();
+    }
+
     return 0;
 }
+
+FizzBuzz::FizzBuzz(int first, int second, int count)
--- Fizz-Buzz/C++/fizz_buzz.h
+++ Fizz-Buzz/C++/fizz_buzz.h
@@ -1,7 +1,21 @@
 #ifndef __FIZZ_BUZZ_H__
 #define __FIZZ_BUZZ_H__
 
+#include <cstdlib>
 #include <fstream>
 #include <iostream>
+
+class FizzBuzz {
+    private:
+        const int first;
+        const int second;
+        const int count;
+    public:
+        FizzBuzz(int first, int second, int count);
+        void set_first(int first);
+        void set_second(int second);
+        void set_count(int count);
+        void evaluate();
+}
 
 #endif
