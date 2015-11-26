699485c7ffb01e7df038c6edcd190bdd32c3b279
aisaacroth <aisaacroth@gmail.com>
2015-11-25 20:26:17 -0500

    Adding test cases.

---
+++ Bit-Positions/C/test-bit-position.c
@@ -0,0 +1,15 @@
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "bit-position.h"
+
+/*
+static void die(const char *message) {
+    perror(message);
+    exit(1);
+}
+*/
+
+int main(int argc, char **argv) {
+    return 0;
+}
