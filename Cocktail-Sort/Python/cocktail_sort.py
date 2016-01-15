6f86aebd9768ec5cc40fbffe78a100b05bca103c
aisaacroth <aisaacroth@gmail.com>
2016-01-15 16:44:16 -0500

    Cocktail sort done. Will document later.

--- Cocktail-Sort/Python/cocktail_sort.py
+++ Cocktail-Sort/Python/cocktail_sort.py
@@ -9,7 +9,56 @@ Date:   2016-01-15
 import sys
 
 def main(args):
-    pass
+    in_file = args[1]
+    line_generator = read_file(in_file)
+
+    try:
+        while True:
+            line = next(line_generator)
+            num_list, iterations = split_line(line)
+            print(cocktail_sort(num_list, iterations))
+    except StopIteration:
+        pass
+
+
+def read_file(filename):
+    assert type(filename) is str
+
+    with open(filename, 'r') as in_file:
+        for line in in_file:
+            yield line.strip()
+
+
+def split_line(line):
+    assert type(line) is str
+
+    split_list = line.split('|')
+    iterations = int(split_list[1])
+
+    nums = [int(i.strip()) for i in split_list[0].split()]
+    return (nums, iterations)
+
+
+def cocktail_sort(num_list, iterations):
+    i = 0
+    stop_right = len(num_list) - 1
+    stop_left = -1
+
+    while i < iterations:
+
+        stop_left += 1
+        for j in range(stop_left, stop_right):
+            if num_list[j] > num_list[j + 1]:
+                num_list[j], num_list[j + 1] = num_list[j + 1], num_list[j]
+
+        stop_right -= 1
+        for j in range(stop_right, stop_left - 1, -1):
+            if num_list[j] > num_list[j + 1]:
+                num_list[j], num_list[j + 1] = num_list[j + 1], num_list[j]
+        
+        i+= 1
+
+    return num_list
 
 
 def print_arguments(arg):
--- Cocktail-Sort/Python/test_cocktail_sort.py
+++ Cocktail-Sort/Python/test_cocktail_sort.py
@@ -7,14 +7,63 @@ Author: Alexander Roth
 Date:   2016-01-15
 '''
 
+from cocktail_sort import cocktail_sort, read_file, split_line
 import unittest
 
 class TestCocktailSort(unittest.TestCase):
 
     def setUp(self):
+        self.filename = 'test.txt'
+        self.one_iter_line = '5 4 9 10 7 3 2 1 6 | 1'
+        self.three_iter_line = '9 8 7 6 5 4 3 2 1 | 3'
         self.one_iter_list = [5, 4, 9, 10, 7, 3, 2, 1, 6]
         self.three_iter_list = [9, 8 , 7, 6, 5, 4, 3, 2, 1]
 
+    def test_read_file(self):
+        test_ans = read_file(self.filename)
+        exp_ans = '5 4 9 10 7 3 2 1 6 | 1'
+        first_iter = next(test_ans)
+
+        self.assertIsNotNone(first_iter)
+        self.assertEqual(exp_ans, first_iter)
+
+        exp_ans = '9 8 7 6 5 4 3 2 1 | 3'
+        second_iter = next(test_ans)
+
+        self.assertIsNotNone(second_iter)
+        self.assertEqual(exp_ans, second_iter)
+
+
+    def test_split_line(self):
+        test_ans = split_line(self.one_iter_line)
+        exp_ans = ([5, 4, 9, 10, 7, 3, 2, 1, 6], 1)
+        
+        self.assertIsNotNone(test_ans)
+        self.assertEqual(exp_ans[0], test_ans[0])
+        self.assertEqual(exp_ans[1], test_ans[1])
+        self.assertEqual(exp_ans, test_ans)
+
+        test_ans = split_line(self.three_iter_line)
+        exp_ans = ([9, 8, 7, 6, 5, 4, 3, 2, 1], 3)
+
+        self.assertIsNotNone(test_ans)
+        self.assertEqual(exp_ans[0], test_ans[0])
+        self.assertEqual(exp_ans[1], test_ans[1])
+        self.assertEqual(exp_ans, test_ans)
+
+    def test_cocktail_sort(self):
+        test_ans = cocktail_sort(self.one_iter_list, 1)
+        exp_ans = [1, 4, 5, 9, 7, 3, 2, 6, 10]
+        
+        self.assertIsNotNone(test_ans)
+        self.assertEqual(exp_ans, test_ans)
+
+        test_ans = cocktail_sort(self.three_iter_list, 3)
+        exp_ans = [1, 2, 3, 6, 5, 4, 7, 8, 9]
+
+        self.assertIsNotNone(test_ans)
+        self.assertEqual(exp_ans, test_ans)
+        
 
 if __name__ == '__main__':
     unittest.main()
